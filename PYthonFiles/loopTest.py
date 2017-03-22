f0894bd936a089b3394cd0601db8a150be3a0619
saurabharas <saurabharas1994@gmail.com>
2017-03-22 14:50:47 +0530

    readMe changes

---
+++ PYthonFiles/loopTest.py
@@ -0,0 +1,8 @@
+'''
+1.Take pickupDate and dropDate and time
+2.divide the day in 4-5 slots of 9-3,9-6....
+3.Take current date and time and put pickUpTime and dropTime in nearby slot
+4.fetch values->increase the dropTime by 3
+
+'''
+while()
